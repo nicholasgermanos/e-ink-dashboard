a9784bbafd70e0950773e6a01b3a05f0dbfb9400
Nick Germanos <nickgermanos@Nicks-MacBook-Pro.local>
2026-01-11 15:59:13 +1100

    more index changes

--- e-ink-frontend/display/views.py
+++ e-ink-frontend/display/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+import datetime
 
 def index(request):
     context = {}
+
+    # Date
+    wd = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    today = datetime.datetime.today()
+    day = wd[today.weekday()]
+    context['day'] = day
+    context['date'] = day, " 12 Jan"
     return render(request, "display/index.html", context)
